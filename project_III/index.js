70b1936af00742a20329aa8c7c9c851d2d304968
clementpicardzivy <clementpicardzivy@gmail.com>
2019-12-09 15:29:28 -0600

    issue with fetch post

--- project_III/index.js
+++ project_III/index.js
@@ -26,48 +26,39 @@ class Backend {
   }
 }
 
-// API.setBaseUrl(baseUrl);
-// API.get(endPoint)
-// .then(data => {
-//   const filtered = data.find(el => el.item === item);
-//   const liDiv = document.createElement('li');
-//   console.log(filtered.item);
-//   liDiv.textContent = filtered.item;
-//   ul.insertAdjacentElement('beforeend', liDiv);
-// })
-
-
 // COPY PASTE TO LEARN JS ONLINE
 const baseUrl = "https://api.learnjavascript.online/demo/";
 const endPoint = "shopping-list.json";
 const form = document.querySelector("#add-item-form");
 const ul = document.querySelector("#shopping-list");
 const API = new Backend;
+API.setBaseUrl(baseUrl);
 
 const displayItem = item => {
     // display the item (string) in the shopping-list element
     ul.insertAdjacentHTML('beforeend', `<li>${item}</li>`);
 }
 
-displayItem("Pomme de pain");
-
-
-/**
- * @param {string} item
- */
 const addItem = item => {
-    // send the item (string) to the API
-    // and display it in the shopping list
-
+    // send the item (string) to the API + and display it in the shopping list
+    API.post(endPoint, item)
+    .then(displayItem(item));
 }
+
 
 const initShoppingList = () => {
     // get list of existing items and display them
+    API.get(endPoint)
+    .then(data => data.forEach(el => displayItem(el)));
 
     // call the addItem(item) method when a user submits the add-item-form
-
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      let toAdd = document.querySelector("#item-name");
+      addItem(toAdd.value)
+      toAdd.textContent = "";
+    })
 }
-
 
 //sample usage (do not remove)
 initShoppingList();
